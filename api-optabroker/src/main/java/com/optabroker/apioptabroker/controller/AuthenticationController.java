6ce3f765fe49bd928cb99a26ca3ea8dbb3940229
Thyagoj7 <t.y_josef7@hotmail.com>
2023-08-03 20:17:06 -0300

    adcionando classe de autenticacao e metodo para criptografar senhar

---
+++ api-optabroker/src/main/java/com/optabroker/apioptabroker/controller/AuthenticationController.java
@@ -0,0 +1,32 @@
+package com.optabroker.apioptabroker.controller;
+
+import com.optabroker.apioptabroker.dto.AuthenticationDTO;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import javax.validation.Valid;
+
+@RestController
+@RequestMapping("auth")
+public class AuthenticationController {
+
+    @Autowired
+    private AuthenticationManager authenticationManager;
+
+    @PostMapping("/login")
+    public ResponseEntity login(@RequestBody @Valid AuthenticationDTO data){
+        var usernamePassword = new UsernamePasswordAuthenticationToken(data.getLogin(), data.getPassword());
+        var auth = this.authenticationManager.authenticate(usernamePassword);
+
+        return ResponseEntity.ok().build();
+
+
+    }
+
+}
