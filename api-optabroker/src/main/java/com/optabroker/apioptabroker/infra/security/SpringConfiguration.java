0339b74aa5f0b9a189351ae3837c8848d2ce2b3c
Thyagoj7 <t.y_josef7@hotmail.com>
2023-09-05 23:43:57 -0300

    geração de token concluida

--- api-optabroker/src/main/java/com/optabroker/apioptabroker/infra/security/SpringConfiguration.java
+++ api-optabroker/src/main/java/com/optabroker/apioptabroker/infra/security/SpringConfiguration.java
@@ -1,5 +1,6 @@
 package com.optabroker.apioptabroker.infra.security;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
@@ -11,10 +12,14 @@ import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
 @Configuration
 @EnableWebSecurity
 public class SpringConfiguration {
+
+    @Autowired
+    SecurityFilter securityFilter;
 
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity httpSecurity) throws Exception{
@@ -25,9 +30,10 @@ public class SpringConfiguration {
                         .antMatchers(HttpMethod.POST, "/auth/login").permitAll() // Qualquer um vai poder disparar requisição para login
                         .antMatchers(HttpMethod.POST, "/auth/register").permitAll() //QUal um vai poder disparar requisição para registrar
                         .antMatchers(HttpMethod.POST, "/adcionar").hasRole("ADMIN")
-
                         .anyRequest().authenticated()
                 )
+
+                .addFilterBefore(securityFilter, UsernamePasswordAuthenticationFilter.class)
                 .build();
     }
 
